c95fd846af8ff5a32ebfa19a37fcb3d05a743e50
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "stream_analytics_clusters_id" {
   description = "Map of id values across all stream_analytics_clusters, keyed the same as var.stream_analytics_clusters"
-  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.id }
+  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "stream_analytics_clusters_location" {
   description = "Map of location values across all stream_analytics_clusters, keyed the same as var.stream_analytics_clusters"
-  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.location }
+  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "stream_analytics_clusters_name" {
   description = "Map of name values across all stream_analytics_clusters, keyed the same as var.stream_analytics_clusters"
-  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.name }
+  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "stream_analytics_clusters_resource_group_name" {
   description = "Map of resource_group_name values across all stream_analytics_clusters, keyed the same as var.stream_analytics_clusters"
-  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.resource_group_name }
+  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "stream_analytics_clusters_streaming_capacity" {
   description = "Map of streaming_capacity values across all stream_analytics_clusters, keyed the same as var.stream_analytics_clusters"
-  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.streaming_capacity }
+  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.streaming_capacity if v.streaming_capacity != null }
 }
 output "stream_analytics_clusters_tags" {
   description = "Map of tags values across all stream_analytics_clusters, keyed the same as var.stream_analytics_clusters"
-  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.tags }
+  value       = { for k, v in azurerm_stream_analytics_cluster.stream_analytics_clusters : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
